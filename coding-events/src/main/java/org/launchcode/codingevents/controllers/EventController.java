8b08b11d133177c87c4844fc3fd5b7c6f0fef657
Tilahun Assefa Demissie <til.assf98@gmail.com>
2021-10-05 19:03:06 -0500

    branch form2 processing form input done

--- coding-events/src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ coding-events/src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -3,7 +3,9 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,20 +13,25 @@ import java.util.List;
 @Controller
 @RequestMapping("events")
 public class EventController {
+   private static List<String> events = new ArrayList<>();
+
+    //respond for localhost:8080/events
     @GetMapping
     public String displayAllEvents(Model model){
-        List<String> events = new ArrayList<>();
-        events.add("Code with pride");
-        events.add("Strange loop");
-        events.add("Apple WWDC");
-        events.add("Spring Platform");
         model.addAttribute("events", events);
         return "events/index";
     }
 
-    //lives at /events/create
+    //lives at localhost:8080/events/create
     @GetMapping("create")
     public String renderCreateEventForm(){
         return "events/create";
+    }
+
+    //lives at localhost:8080/events/create but for post
+    @PostMapping("create")
+    public String processCreateEventForm(@RequestParam String eventName){
+        events.add(eventName);
+        return "redirect:";
     }
 }
