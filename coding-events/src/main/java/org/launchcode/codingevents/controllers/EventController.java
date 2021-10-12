755fa302a06793d7a773fb309f1d74d172f4b26f
Tilahun Assefa Demissie <til.assf98@gmail.com>
2021-10-12 03:06:55 -0500

    creating data layer done

--- coding-events/src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ coding-events/src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,5 +1,6 @@
 package org.launchcode.codingevents.controllers;
 
+import org.launchcode.codingevents.data.EventData;
 import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,12 +15,11 @@ import java.util.List;
 @Controller
 @RequestMapping("events")
 public class EventController {
-   private static List<Event> events = new ArrayList<>();
 
     //respond for localhost:8080/events
     @GetMapping
     public String displayAllEvents(Model model){
-        model.addAttribute("events", events);
+        model.addAttribute("events", EventData.getAll());
         return "events/index";
     }
 
@@ -32,7 +32,7 @@ public class EventController {
     //lives at localhost:8080/events/create but for post
     @PostMapping("create")
     public String processCreateEventForm(@RequestParam String eventName, @RequestParam String eventDescription){
-        events.add(new Event(eventName, eventDescription));
+        EventData.add(new Event(eventName, eventDescription));
         return "redirect:";
     }
 }
