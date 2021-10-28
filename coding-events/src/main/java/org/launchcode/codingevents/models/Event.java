30715a4255f8e8e25281db5839894d0ba07e75dd
Tilahun Assefa Demissie <til.assf98@gmail.com>
2021-10-28 17:56:57 -0500

    18.2 creating the one to one relationship done

--- coding-events/src/main/java/org/launchcode/codingevents/models/Event.java
+++ coding-events/src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,6 +1,7 @@
 package org.launchcode.codingevents.models;
 
 import javax.persistence.*;
+import javax.validation.Valid;
 import javax.validation.constraints.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -14,33 +15,32 @@ public class Event extends AbstractEntity {
     @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
     private String name;
 
-    @Size(max = 500, message = "Description too long!")
-    private String description;
-
-    @Email(message = "Invalid email. Try again.")
-    private String contactEmail;
-
-    @NotBlank(message = "Location can not be blank!")
-    private String location;
+    @OneToOne(cascade = CascadeType.ALL)
+    @Valid
+    @NotNull
+    @JoinColumn(name = "event_details_id")
+    private EventDetails eventDetails;
 
     @AssertTrue(message = "Attendee must register for the event")
     private boolean registered;
-
-    @Positive(message = "Number of attendees must be one or more.")
-    private int numberOfAttendee;
 
     @ManyToOne
     @JoinColumn(name = "event_category_id")
     @NotNull(message = "Category is required")
     private EventCategory eventCategory;
 
-    public Event() { }
+    public EventDetails getEventDetails() {
+        return eventDetails;    }
 
-    public Event(String name, String description, String contactEmail, String location, boolean registered, EventCategory eventCategory) {
+    public void setEventDetails(EventDetails eventDetails) {
+        this.eventDetails = eventDetails;
+    }
+
+    public Event() {
+    }
+
+    public Event(String name, boolean registered, EventCategory eventCategory) {
         this.name = name;
-        this.description = description;
-        this.contactEmail = contactEmail;
-        this.location = location;
         this.registered = registered;
         this.eventCategory = eventCategory;
     }
@@ -53,44 +53,12 @@ public class Event extends AbstractEntity {
         this.name = name;
     }
 
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getContactEmail() {
-        return contactEmail;
-    }
-
-    public void setContactEmail(String contactEmail) {
-        this.contactEmail = contactEmail;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
+    public boolean isRegistered() {
+        return registered;
     }
 
     public void setRegistered(boolean registered) {
         this.registered = registered;
-    }
-
-    public int getNumberOfAttendee() {
-        return numberOfAttendee;
-    }
-
-    public void setNumberOfAttendee(int numberOfAttendee) {
-        this.numberOfAttendee = numberOfAttendee;
-    }
-
-    public boolean isRegistered() {
-        return registered;
     }
 
     public EventCategory getEventCategory() {
---
+++ coding-events/src/main/java/org/launchcode/codingevents/models/EventDetails.java
@@ -0,0 +1,75 @@
+package org.launchcode.codingevents.models;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
+@Entity
+public class EventDetails extends AbstractEntity{
+
+    @Size(max = 500, message = "Description too long!")
+    private String description;
+
+    @Email(message = "Invalid email. Try again.")
+    private String contactEmail;
+
+    @NotBlank(message = "Location can not be blank!")
+    private String location;
+
+    @Positive(message = "Number of attendees must be one or more.")
+    private int numberOfAttendee;
+
+
+    @OneToOne(mappedBy = "eventDetails")
+    @JoinColumn(name = "event_id")
+    private Event event;
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public EventDetails(){}
+
+    public EventDetails(String description, String contactEmail, String location, int numberOfAttendee) {
+        this.description = description;
+        this.contactEmail = contactEmail;
+        this.location = location;
+        this.numberOfAttendee = numberOfAttendee;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getNumberOfAttendee() {
+        return numberOfAttendee;
+    }
+
+    public void setNumberOfAttendee(int numberOfAttendee) {
+        this.numberOfAttendee = numberOfAttendee;
+    }
+}
